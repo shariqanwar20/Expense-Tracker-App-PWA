705343f835d4f68a03746a271407ca51e3d55ddd
Shariq <shariqanwar20@hotmail.com>
2020-12-25 22:04:14 +0500

    implemented expense app and added push notifications functionality and caching files functionality

---
+++ public/serviceWorker.js
@@ -0,0 +1,34 @@
+const filesToCache = [
+    '/',
+    './index.html',
+    '/static/js/bundle.js',
+    '/static/js/0.chunk.js',
+    '/static/js/main.chunk.js',
+    './serviceWorker.js',
+    './manifest.json',
+    './logo192.png',
+    './logo512.png',
+    './favicon.ico'
+]
+
+this.addEventListener('activate', (e) => {
+    console.log('[Service Worker] activated')
+})
+
+this.addEventListener('install', (e) => {
+    console.log('[Service Worker] installed');
+    e.waitUntil(
+        caches.open("expense tracker").then((cache) => {
+            console.log("caching added files");
+            return (cache.addAll(filesToCache))
+        })
+    );
+})
+
+this.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((respone) => {
+            return respone || fetch(e.request)
+        })
+    )
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import { AddTransaction } from './Components/AddTransaction';
 import { Balance } from './Components/Balance';
@@ -6,8 +6,31 @@ import { Header } from './Components/Header';
 import { IncomeExpense } from './Components/IncomeExpense';
 import { TransactionHistory } from './Components/TransactionHistory';
 import { GlobalContext } from './Services/GlobalContext';
+import firebase from "./Services/firebase";
 
 function App() {
+  //useEffect is used so that permission is asked once and token is also logged once
+  useEffect(() => {
+    const messaging = firebase.messaging();
+    Notification.requestPermission().then((permission) => {
+      console.log("permission", permission);
+      if (permission === "granted")
+      {
+        messaging.getToken().then((token) => {
+          console.log("Token: ");
+          console.log(token);  
+        })
+        .catch((err) => {
+          console.log("Error", err);
+        })
+      }
+    })
+    .catch((err) => {
+      console.log("Error: ", err);
+      
+    })
+  }, [])
+
   const [income, setIncome] = useState(0);
   const [expense, setExpense] = useState(0);
   const [balance, setBalance] = useState(0);
